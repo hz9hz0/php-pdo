57261ec085ac46da5986b881bc06f1041e42a49c
Hossen Zareipour <hossein.zareipour.hz@gmail.com>
2023-11-02 05:41:39 +0330

    Update db.php
    add delete method

--- db.php
+++ db.php
@@ -2,85 +2,79 @@
 
 class db
 {
-    protected $Pdo;
-    private $dbUser;
-    private $dbPass;
-    private $dbName;
-    private $dbHost;
-    // creat new connection
-    public function __construct($dbHost, $dbName, $dbUser, $dbPass){
+    public $Pdo;
 
-        $dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+    public function __construct ($dbName, $dbUser, $dbPass)
+    {
+        $dsn = "mysql:host=localhost;dbname=$this->dbName;charset=UTF8";
 
         try {
-            $Pdo = new PDO($dsn, $dbUser, $dbPass);
+            $Pdo = new PDO($dsn, $this->dbUser, $this->dbPass);
             $Pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->Pdo = $Pdo;
-            //if ($pdo) {
-            //    echo "Connected to the $dbName database successfully!";
-            //}
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
-
     }
-    //
+    //===========================
     function __destruct() {
-        $this->Pdo = null;
+        unset($this->Pdo);
     }
+    //============================
     // get Where for sql
     private function creatWhere($values=[])
-        {
-            $where = '';
-            $valueString = [];
-            $fieldString = null;
-            if(is_array($values)){
-                foreach($values as $value){
-                    if($where!=''){
-                        $where.=" AND";
-                    }
-                    if(count($value)==3){
-                        $name = $value[0];
-                        $opreator = $value[1];
-                        $val = $value[2];
-                        $valueString[":".$name.""]="{$val}";
-                        $fieldString =" `{$name}`";
-                        $where.=$fieldString." ".$opreator." :".$name;
-                    }
+    {
+        $where = '';
+        $valueString = [];
+        $fieldString = null;
+        if(is_array($values)){
+            foreach($values as $value){
+                if($where!=''){
+                    $where.=" AND";
+                }
+                if(count($value)==3){
+                    $name = $value[0];
+                    $opreator = $value[1];
+                    $val = $value[2];
+                    $valueString[":".$name.""]="{$val}";
+                    $fieldString =" `{$name}`";
+                    $where.=$fieldString." ".$opreator." :".$name;
                 }
             }
-            if($where == ''){
-                $where = ' 0';// not thing for where
-                if($values=='all'){
-                    $where = ' 1';// not thing for where
-                }
-            }
-            $result = [$where,$valueString];
-            return $result;
         }
+        if($where == ''){
+            $where = ' 0';
+            if($values=='all'){
+                $where = ' 1';
+            }
+        }
+        $result = [$where,$valueString];
+        return $result;
+    }
     // get Connection
     public function getmyDB()
-        {
+    {
         if ($this->Pdo instanceof PDO)
-            {
+        {
             return $this->Pdo;
-            }
         }
+    }
     // set Connection
     public function setmyDB(PDO $data)
     {
         $this->Pdo = $data;
     }
     // insert
-    public function insert($options=[],$table){
+    public function insert(array $options,string $table){
         global $pdo;
-        
+
         $queryString = "";
         $p    = count($options);
         $start   = 0;
         $fieldString = null;
         $valueString = array();
         // creat sql query
+        $vs = '';
         foreach($options as $key=>$val){
             $fieldString.=" `{$key}`";
             $valueString[":".$key.""]="{$val}";
@@ -100,13 +94,13 @@ class db
             echo $e->getMessage();
         }
     }
-    
+
     // update
-    public function update($options=[],$table,$where=[]){
-        
+    public function update(array $options,string $table,array $where){
+
         $queryString = "";
-        $fieldString = ""; 
-        $valueString = array();  
+        $fieldString = "";
+        $valueString = array();
         $p           = count($options);
         $start       = 0;
         foreach($options as $key=>$val){
@@ -119,24 +113,42 @@ class db
             }
             $start++;
         }
-        // get where 
+        // get where
         $creatWhere = $this->creatWhere($where);
         $queryString = "UPDATE `{$table}` SET {$fieldString} WHERE".$creatWhere[0];
         try {
-             
+
             $stmt = $this->Pdo->prepare($queryString);
             $valueString = array_merge($valueString,$creatWhere[1]);
-            $stmt->execute($valueString);
+            $result = $stmt->execute($valueString);
+            return $result;
         }catch (PDOException $e) {
-            
-            echo $e->getMessage();
+            return $e->getMessage();
         }
     }
     //---------
-    public function select($options=[],$table,$where=[]){
+    // delete
+    public function DELETE(string $table, array $where){
+        $valueString = array();
+        // get where
+        $creatWhere = $this->creatWhere($where);
+        if($creatWhere == '1')
+            $creatWhere = '0';
+        $queryString = "DELETE FROM `{$table}` WHERE".$creatWhere[0];
+        try {
+
+            $stmt = $this->Pdo->prepare($queryString);
+            $valueString = array_merge($valueString,$creatWhere[1]);
+            $result = $stmt->execute($valueString);
+        }catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    //---------
+    public function select(array $options, string $table, array|string $where){
         $queryString = "";
-        $fieldString = ""; 
-        $valueString = array();  
+        $fieldString = "";
+        $valueString = array();
         $p           = count($options);
         $start       = 0;
         // select name1, name2, ...
@@ -162,7 +174,7 @@ class db
             $stmt->execute($valueString);
             return $stmt;
         }catch (PDOException $e) {
-            
+
             echo $e->getMessage();
         }
     }
@@ -171,5 +183,8 @@ class db
 
         return $respons_query->fetchAll(PDO::FETCH_ASSOC);
     }
+    //--------
+    public function query($sql){
+        return $this->Pdo->query($sql);
+    }
 }
-?>
